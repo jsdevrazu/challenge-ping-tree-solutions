4a03864e57d5b5e2ba8acda5c562c8bf69edf9c3
jsdevrazu <islamrajulearn.dev@gmail.com>
2023-04-06 22:52:05 +0600

    tried post method with servertest

--- test/endpoints.js
+++ test/endpoints.js
@@ -2,8 +2,32 @@ process.env.NODE_ENV = 'test'
 
 var test = require('ava')
 var servertest = require('servertest')
-
+var bl = require('bl')
 var server = require('../lib/server')
+
+const newTarget = {
+  id: '123',
+  url: 'http://example.com',
+  value: 0.5,
+  maxAcceptsPerDay: 10,
+  accept: {
+    geoState: {
+      $in: ['ca', 'ny']
+    },
+    hour: {
+      $in: ['13', '14', '15']
+    }
+  }
+}
+const options = {
+  method: 'POST',
+  // url: '/api/targets',
+  headers: {
+    'Content-Type': 'application/json'
+  },
+  encoding: 'json',
+  body: JSON.stringify(newTarget)
+}
 
 test.serial.cb('healthcheck', function (t) {
   var url = '/health'
@@ -15,3 +39,15 @@ test.serial.cb('healthcheck', function (t) {
     t.end()
   })
 })
+
+// test.serial.cb('post a target', function (t) {
+//   const streamData = servertest(server(), '/api/targets', options)
+
+//   streamData.on('data', (err, res) => {
+//     console.log(err)
+//     console.log(res)
+//     t.falsy(err, 'no error')
+
+//     t.end()
+//   })
+// })
